f2f6f42c6d9bc441300c93bebf4bb0f8d83536a6
Tiago <tiagopereira14200@gmail.com>
2024-08-25 11:27:15 -0300

    ajuste

--- ambiente/index.js
+++ ambiente/index.js
@@ -1,29 +1,24 @@
-document.getElementById("bot-menu").addEventListener("click", function() {
-    const menu = document.getElementById("menu");
-  
-    if (menu.style.display === "flex") {
-      menu.style.display = "none";  
-    } else {
-      menu.style.display = "flex"; 
-    }
-  });
+document.getElementById("bot-menu").addEventListener("click", function () {
+  const menu = document.getElementById("menu");
 
-  window.addEventListener("resize", function() {
-    const menu = document.getElementById("menu");
-      menu.style.display = "none";
-  });
-  
-  function adjustBoxPosition() {
-    const box = document.querySelector('.vertical-menu');
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-  
-    // Define a posição do box como uma fração do tamanho da janela
-    // box.style.top = (windowHeight * 0.) + 'px'; // 30% da altura da janela
-    box.style.right = (windowWidth * 0.1) + 'px'; // 30% da largura da janela
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
   }
+});
 
-  adjustBoxPosition();
+window.addEventListener("resize", function () {
+  const menu = document.getElementById("menu");
+  menu.style.display = "none";
+});
 
-// Adiciona um listener para ajustar a posição quando a janela é redimensionada
-window.addEventListener('resize', adjustBoxPosition)
+function adjustBoxPosition() {
+  const box = document.querySelector(".vertical-menu");
+  const windowWidth = window.innerWidth;
+
+  box.style.width = windowWidth * 0.4 + "px";
+}
+
+adjustBoxPosition();
+window.addEventListener("resize", adjustBoxPosition);
