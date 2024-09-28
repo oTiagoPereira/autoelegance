9cd0167a083988f0c2e3f00aca0323397e358416
Tiago <tiagopereira14200@gmail.com>
2024-09-27 23:07:08 -0300

    ajustes

--- ambiente/index.js
+++ ambiente/index.js
@@ -1,16 +1,16 @@
 const menu = document.getElementById("menu");
-let menuItems = document.querySelectorAll(".menuItens");
+let menuItens = document.querySelectorAll(".menuItens");
 
 function mostraMenu() {
-  menuItems.forEach(function(item) {
+  menuItens.forEach(function (item) {
     item.addEventListener("click", function () {
-      menu.style.width = "0"; 
+      menu.style.width = "0";
     });
   });
   if (menu.style.width && menu.style.width !== "0px") {
     menu.style.width = "0";
   } else {
-    menu.style.width = '250px';
-    menu.style.display = 'flex';
+    menu.style.display = "flex";
+    menu.style.width = "250px";
   }
 }
